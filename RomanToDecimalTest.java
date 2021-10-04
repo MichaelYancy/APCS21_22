52b2f63a932db7c79f1df894b4649c3069e475dd
MichaelYancy <89647833+MichaelYancy@users.noreply.github.com>
2021-10-03 21:03:52 -0400

    Add files via upload

---
+++ RomanToDecimalTest.java
@@ -0,0 +1,34 @@
+import static org.junit.Assert.*;
+
+public class RomanToDecimalTest {
+
+    @org.junit.Test
+    /**
+     * Tests args for class RTD
+     */
+    public void testRomanToDecimalTest() {
+        assertEquals(RomanToDecimal.romanToDecimal("XI"), 11);
+        assertEquals(RomanToDecimal.romanToDecimal("IXIX"), 20);
+        assertEquals(RomanToDecimal.romanToDecimal("MCIX"), 1109);
+        assertEquals(RomanToDecimal.romanToDecimal("CM"), 900);
+        assertEquals(RomanToDecimal.romanToDecimal("CL"), 150);
+        assertEquals(RomanToDecimal.romanToDecimal("XXII"), 22);
+        assertEquals(RomanToDecimal.romanToDecimal("XLVI"), 46);
+        assertEquals(RomanToDecimal.romanToDecimal("IX"), 9);
+        assertEquals(RomanToDecimal.romanToDecimal("CD"), 400);
+        assertEquals(RomanToDecimal.romanToDecimal("DCL"), 650);
+
+        assertEquals(RomanToDecimal.romanToDecimal("Ur mom"), -1);
+        assertEquals(RomanToDecimal.romanToDecimal("Whoopsies"), -1);
+        assertEquals(RomanToDecimal.romanToDecimal("Gosling"), -1);
+        assertEquals(RomanToDecimal.romanToDecimal("JAMES"), -1);
+
+        assertEquals(RomanToDecimal.romanToDecimal("ICM"), 901);
+        assertEquals(RomanToDecimal.romanToDecimal("IVIVIV"), 16);
+        assertEquals(RomanToDecimal.romanToDecimal("ID"), 501);
+        assertEquals(RomanToDecimal.romanToDecimal("IIIX"), 11);
+
+        assertNotEquals(RomanToDecimal.romanToDecimal("Joe"), 20);
+        assertNotEquals(RomanToDecimal.romanToDecimal("ur mom"), 20);
+    }
+}
